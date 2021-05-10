e22c184b473bb08aea171d9662d5b46543223fc6
Cynthia Z E MacLeod (C3D) <Cynthia.MacLeod@cloud3d.ltd>
2021-05-10 22:54:18 +0100

    Move extensions to common projects

---
+++ ConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration.Configuration;
+
+namespace System.Data.Entity.ModelConfiguration
+{
+    public static class ConfigurationExtensions
+    {
+        public static ConventionPrimitivePropertyConfiguration HasIndex(this ConventionPrimitivePropertyConfiguration configuration, IndexAttribute index = null)
+        {
+            return configuration.HasColumnAnnotation(IndexAnnotation.AnnotationName, new IndexAnnotation(index ?? new IndexAttribute()));
+        }
+
+        public static PrimitivePropertyConfiguration HasIndex(this PrimitivePropertyConfiguration configuration, IndexAttribute index = null)
+        {
+            return configuration.HasColumnAnnotation(IndexAnnotation.AnnotationName, new IndexAnnotation(index ?? new IndexAttribute()));
+        }
+
+        public static ConventionPrimitivePropertyConfiguration HasIndexes(this ConventionPrimitivePropertyConfiguration configuration, params IndexAttribute[] indexes)
+        {
+            return configuration.HasColumnAnnotation(IndexAnnotation.AnnotationName, new IndexAnnotation(indexes));
+        }
+
+        public static PrimitivePropertyConfiguration HasIndexes(this PrimitivePropertyConfiguration configuration, params IndexAttribute[] indexes)
+        {
+            return configuration.HasColumnAnnotation(IndexAnnotation.AnnotationName, new IndexAnnotation(indexes));
+        }
+    }
+}
